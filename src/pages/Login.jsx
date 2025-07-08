2c3049ed8dc8ab6fb1e1ae43065960ece1ab97fd
SridharshanY <sridharshany.2001@gmail.com>
2025-07-08 17:55:20 +0530

    made a backend server

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,30 @@
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="flex justify-center items-center">
+      <form className="mt-20 flex flex-col justify-center gap-4 items-center border-2 border-black rounded-xl p-6 w-2xl">
+        <h1 className="text-4xl font-semibold">Login</h1>
+        <input
+          type="email"
+          placeholder="Email ID"
+          className="w-full px-4 py-2 border rounded-lg"
+          name="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full px-4 py-2 border rounded-lg"
+          name="password"
+        />
+        <button
+          type="submit"
+          className="text-red-600 bg-white w-full py-2 rounded-lg border border-red-600 hover:bg-red-600 hover:text-white transition 
+          hover:cursor-pointer"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
