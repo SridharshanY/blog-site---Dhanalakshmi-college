a4995c5bc35ac4e429c4556d6acead7ab88cd301
SridharshanY <sridharshany.2001@gmail.com>
2025-07-09 18:09:11 +0530

    conncted to database

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
-app.listen(3000, ()=>{
-    console.log("Server is running")
-})
+const uri =
+  "mongodb+srv://webdeveloper05ats:S0JPIa3qojZl5Ofo@cluster0.qwgdii2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Database is connected");
+    app.listen(3000, () => {
+      console.log("Server is running");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
